f0e11aac3e64612c39d77690c5d8c4522bae7e28
Milinn Kremitzki <milinngraser@gmail.com>
2019-04-15 16:46:57 -0500

    finished user-signup

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ def sign_up():
      verify_pwd_arg=request.form["verify_pwd"]
      email_arg=request.form["email"]
      if not username_arg.isalnum() or len(username_arg)<3 or len(username_arg)>20:
-         nameerror_arg = "That is not a valid username."
+         nameerror_arg = "Please enter a valid username"
          return render_template("index.html", email=email_arg, nameerror=nameerror_arg)
      if not password_arg.isalnum() or len(password_arg)<3 or len(password_arg)>20:
-         passworderror_arg = "That is not a valid password."
+         passworderror_arg = "Please enter a valid password"
          return render_template("index.html", username=username_arg, email=email_arg, pwderror=passworderror_arg)
      if password_arg != verify_pwd_arg:
-         verify_pwd_error_arg = "Those passwords don't match."
+         verify_pwd_error_arg = "Those passwords don't match"
          return render_template("index.html", username=username_arg, email=email_arg, verify_pwd_error=verify_pwd_error_arg)
      if email_arg == "":
         return render_template("welcome.html", username=username_arg)
@@ -32,7 +32,7 @@ def sign_up():
      count_dot = email_arg.count(".")
      count_space = email_arg.count(" ")
      if len(email_arg)<3 or len(email_arg)>20 or count_at_sym !=1 or count_dot!=1 or count_space!=0:
-        verify_email_error_arg = "That isn't a valid email address."
+        verify_email_error_arg = "Enter a valid email address"
         return render_template("index.html", username=username_arg, verify_email=verify_email_error_arg)
 
      
@@ -41,9 +41,7 @@ def sign_up():
 
 
 
-#get and check email and assign error if necessary
 
-#if no errors, reirect to welome page
 
 @app.route("/")
 def index():
